f2ceb6d83cc71b51c5002bdbb9aaa0cc0061025f
Sergio Ruiz <sergio.rruvalcaba@alumnos.udg.mx>
2020-10-21 00:23:13 -0500

    prueba01

--- burbuja.go
+++ burbuja.go
@@ -1,10 +1,32 @@
 package main
 
-func Burbuja(s []int64)  {
+import "fmt"
 
+func burbuja(listaDeNumeros []int64){
+	var aux int64
+
+	for i := 0; i < len(listaDeNumeros); i++ {
+		for j := 0; j < len(listaDeNumeros); j++ {
+			if listaDeNumeros[i] < listaDeNumeros[j] {
+				aux = listaDeNumeros[i]
+				listaDeNumeros[i] = listaDeNumeros[j]
+				listaDeNumeros[j] = aux
+			}
+		}
+	}
+	//return listaDeNumeros
 }
 
-func main()  {	
-	var a int
-	a = 1+1
+func main(){
+	var n int
+	var numeros int64
+	var s []int64 
+	fmt.Scan(&n)
+	for i :=0; i<n; i++  {
+		fmt.Scan(&numeros)
+		s = append(s, numeros)
+		
+	}
+
+	//fmt.Println(burbuja(s)) 
 }
